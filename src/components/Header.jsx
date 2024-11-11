85d484265d2d8a8fb78768592ae1b9d4bd56ba50
Cassper84 <onopeter121@gmail.com>
2024-11-11 15:06:49 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/Investment-calculator-logo.png";
+const Header = () => {
+    return <header id="header">
+        <img src={logo} alt="logo showing a money bag" />
+        <h1>Investment Calculator</h1>
+    </header>;
+}
+ 
+export default Header;
